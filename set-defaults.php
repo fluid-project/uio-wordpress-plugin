c3d04b0934b7728dc1ce3906a9ec9d824c37c7fb
Ned Zimmerman <ned@bight.ca>
2019-05-08 10:27:43 -0300

    Refactor to conform to WordPress Coding Standards [WIP]

--- set-defaults.php
+++ set-defaults.php
@@ -1,18 +1,25 @@
 <?php
-/*
-Copyright 2015 OCAD University
+/**
+ * Copyright 2015–2019 OCAD University
+ *
+ * Licensed under the Educational Community License (ECL), Version 2.0 or the New
+ * BSD license. You may not use this file except in compliance with one these
+ * Licenses.
+ *
+ * You may obtain a copy of the ECL 2.0 License and BSD License at
+ * https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+ *
+ * @package User_Interface_Options
+ */
 
-Licensed under the Educational Community License (ECL), Version 2.0 or the New
-BSD license. You may not use this file except in compliance with one these
-Licenses.
-
-You may obtain a copy of the ECL 2.0 License and BSD License at
-https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
-*/
-
-if ( !get_option('uio_template_selector') ) {
-    update_option( 'uio_template_selector', 'body' );
-}
-if ( !get_option('uio_toc_selector') ) {
-    update_option( 'uio_toc_selector', '#main' );
+/**
+ * Set defaults upon plugin activation.
+ */
+function uio_set_defaults() {
+	if ( ! get_option( 'uio_template_selector' ) ) {
+		update_option( 'uio_template_selector', 'body' );
+	}
+	if ( ! get_option( 'uio_toc_selector' ) ) {
+		update_option( 'uio_toc_selector', '#main' );
+	}
 }
